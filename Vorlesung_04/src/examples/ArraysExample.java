c6af38a396b81374c87c9bde61c92762e59517fc
Michael Uhl <michael.b.uhl@gmail.com>
2018-12-06 19:36:25 +0100

    Removed unused variable.

---
+++ Vorlesung_04/src/examples/ArraysExample.java
@@ -0,0 +1,15 @@
+package examples;
+
+import java.util.Arrays;
+
+public class ArraysExample {
+
+	public static void main(String[] args) {
+
+		String[] bierkasten = new String[12];
+		bierkasten[0] = "bier";
+		bierkasten[1] = "kolben";
+
+		System.out.println(Arrays.toString(bierkasten));
+	}
+}
