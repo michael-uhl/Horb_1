43ba04c11f7d6226521cc3e6b869af2e597c73f2
Michael Uhl <michael.b.uhl@gmail.com>
2022-02-04 16:03:00 +0100

    Corrected.

--- Vorlesung_04/src/examples/LottoZiehung.java
+++ Vorlesung_04/src/examples/LottoZiehung.java
@@ -1,9 +1,5 @@
 package examples;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Hashtable;
-import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -13,17 +9,17 @@ public class LottoZiehung {
 		// Lottozahlen erzeugen
 		while (zahlen.size() < 6) {
 			int num = get_1_aus_49();
-			if (zahlen.add(new Integer(num))) {
+			if (zahlen.add(Integer.valueOf(num))) {
 				System.out.println("Neue Zahl " + num);
 			} else {
 				System.out.println("Doppelte Zahl " + num);
 			}
 		}
-		
+
 		// Lottozahlen ausgeben
 		System.out.println(zahlen);
 	}
-	
+
 	private static int get_1_aus_49() {
 		return (int) (Math.random() * 49) + 1;
 	}
