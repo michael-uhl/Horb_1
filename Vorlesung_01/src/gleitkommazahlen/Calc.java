6dc266d4a3ae4ca85d0a59b298e948cd0220f288
Michael Uhl <michael.b.uhl@gmail.com>
2022-02-04 15:59:25 +0100

    Floating Calc.

---
+++ Vorlesung_01/src/gleitkommazahlen/Calc.java
@@ -0,0 +1,11 @@
+package gleitkommazahlen;
+
+public class Calc {
+	public static void main(String[] args) {
+		System.out.println(3/4);
+		System.out.println(3%4);
+
+		System.out.println("123" + "4" == "1234");
+
+	}
+}
