68d4fc9a3a9c5b06f6e8b317f24c8b099fce550a
Michael Uhl <michael.b.uhl@gmail.com>
2022-12-08 17:06:01 +0100

    Umlaut korrigiert.

--- Vorlesung_10/src/a02_Formats/BoolOut.java
+++ Vorlesung_10/src/a02_Formats/BoolOut.java
@@ -1,13 +1,13 @@
 package a02_Formats;
 
 public class BoolOut {
-	public static void main(String ... args) {
-		System.out.printf("Wert für 'wahr': %b.%n", true);
-		System.out.printf("Wert für 'falsch': %b.%n", false);
-		
-		System.out.printf("Fehler: %b.%n", 12);
-		System.out.printf("Fehler: %b.%n", 0);
-		System.out.printf("Fehler: %b.%n", "afdfd");
-		System.out.printf("Fehler: %b.%n", null);
-	}
+    public static void main(String ... args) {
+        System.out.printf("Wert f√ºr 'wahr': %b.%n", true);
+        System.out.printf("Wert f√ºr 'falsch': %b.%n", false);
+
+        System.out.printf("Fehler: %b.%n", 12);
+        System.out.printf("Fehler: %b.%n", 0);
+        System.out.printf("Fehler: %b.%n", "afdfd");
+        System.out.printf("Fehler: %b.%n", null);
+    }
 }
